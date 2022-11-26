760212f098003ed7929887317ce2bdab219d0346
ilia-io <likeapu@gmail.com>
2022-11-26 19:25:40 +0300

    add mui album template

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 import './firebase'
 
@@ -19,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
