9cb711db540362a784c86ab4730da53b6d90bd2f
ilia-io <likeapu@gmail.com>
2022-11-26 17:43:08 +0300

    firebase config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import './firebase'
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
